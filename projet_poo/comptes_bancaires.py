06dc32081b5ae66333ace5d1b15b1b8074f896f4
CapitaineCouscous <103769157+CapitaineCouscous@users.noreply.github.com>
2022-11-17 11:56:22 +0100

    Add files via upload

---
+++ projet_poo/comptes_bancaires.py
@@ -0,0 +1,114 @@
+﻿class CompteBancaire:
+    """Classe défini par :
+            - son numéro
+            - son solde """
+
+    def __init__(self,numero,solde=0):
+        """Créer un nouveau Compte()"""
+        self.solde = solde
+        self.numero = numero
+
+    def get_solde(self):
+        """Obtenir le solde du compte"""
+        return self.solde
+
+    def get_numero(self):
+        """Obtenir le numero du compte"""
+        return self.numero
+
+    def verser(self,nb):
+        """Verser un montant sur un compte"""
+        nb = max(0,nb)
+        self.solde += nb
+
+    def retirer(self,nb):
+        """Retirer un montant sur un compte"""
+        nb = max(0,nb)
+        self.solde -= nb
+
+    def transferer(self,compte,nb):
+        """Transférer un montant d’un compte sur un autre compte"""
+        nb = max(0,nb)
+        self.solde -= nb
+        compte.solde += nb
+
+
+
+class Client:
+    """Classe défini par :
+            - son nom
+            - son adresse
+            - ses comptes """
+
+    def __init__(self,nom,adresse,compte):
+        """Créer un nouveau Client()"""
+        self.nom = nom
+        self.adresse = adresse
+        self.comptes = [compte]
+
+    def get_nom(self):
+        """Obtenir le nom du client"""
+        return self.nom
+
+    def get_adresse(self):
+        """Obtenir l'adresse du client"""
+        return self.adresse
+
+    def get_comptes(self):
+        """Obtenir les comptes du client"""
+        return self.comptes
+
+    def update_comptes(self,nouv_compte):
+        """Mettre à jour la liste des comptes"""
+        self.comptes += [nouv_compte]
+
+
+
+class Banque:
+    def __init__(self):
+        """Classe défini par :
+            - ses clients """
+        self.clients = []
+
+    def get_clients(self):
+        """Obtenir la liste des clients"""
+        return self.clients
+
+    def creer_compte(self,nom,adresse,numero_compte,solde_initial=0):
+        """Créer un nouveau compte"""
+        nouv_compte = CompteBancaire(numero_compte,solde_initial)
+        for client in self.clients:
+            if client.get_nom() == nom and client.get_adresse() == adresse:
+                client.update_comptes(nouv_compte)
+                break
+        else:
+            self.clients.append(Client(nom,adresse,nouv_compte))
+
+    def get_client(self,nom,adresse):
+        """Renvoie le client si il est client de la banque sinon False"""
+        for client in self.clients:
+            if client.get_nom() == nom and client.get_adresse() == adresse:
+                return client
+                break
+        else:
+            return False
+
+    def get_liste_comptes(self,nom,adresse):
+        """Renvoie la liste des comptes d'un client"""
+        client = self.get_client(nom,adresse)
+        if client:
+            return client.get_comptes()
+        else:
+            return client
+
+    def supprimer_compte(self,nom,adresse,numero_compte):
+        """Fonctionne peut-être"""
+        client = self.get_client(nom,adresse)
+        if client:
+            for i in range(len(client.comptes)):
+                if client.comptes[i].numero == numero_compte:
+                    client.comptes.pop(i)
+                    return True
+                break
+        else:
+            return False
---
+++ projet_poo/projet.py
@@ -0,0 +1,123 @@
+﻿import comptes_bancaires as cb
+
+class Chambre :
+    """
+    Chaque instance de Chambre possède comme attributs :
+    -	Un numéro
+    -	Un étage
+    -	Une capacité (de 1 à 3 personnes)
+    -	Un état de propreté (ménage fait ou non)
+    -	Un état d’occupation (libre ou non)
+    -	Un tarif
+    -	Une vue mer ou non
+
+    La classe Chambre est pourvue des méthodes suivantes :
+    -	nettoyer()
+    -	prendre()
+    -	liberer()
+    -	payer()
+    -	Des méthodes permettant d’accéder à chacun des attributs.
+    """
+    def __init__(self, numero:int, etage:int, capacite:int, tarif:float, a_vue_mer:bool) :
+        """Créer une nouvelle Chambre()"""
+        self.numero = numero
+        self.etage = etage
+        self.capacite = max(min(capacite,3),1)
+        self.est_propre = True
+        self.est_occupee = False
+        self.tarif = tarif
+        self.a_vue_mer = a_vue_mer
+
+    def get_numero(self):
+        return self.numero
+
+    def get_etage(self):
+        return self.etage
+
+    def get_capacite(self):
+        return self.capacite
+
+    def get_est_propre(self):
+        return self.est_propre
+
+    def get_est_occupee(self):
+        return self.est_occupee
+
+    def get_tarif(self):
+        return self.tarif
+
+    def get_a_vue_mer(self):
+        return self.a_vue_mer
+
+    def nettoyer(self) :
+        """Déclare la chambre propre"""
+        self.est_propre = True
+        return None
+
+    def	prendre(self) :
+        """Déclare la chambre occupée"""
+        self.est_occupee = True
+        return None
+
+    def	liberer(self) :
+        """Déclare la chambre non-occupée"""
+        self.est_occupee = False
+        return None
+
+##    def	payer(self) :
+##        """"""
+##        pass
+##        return None
+
+
+
+class Hotel :
+    """
+    Chaque instance de Hotel possède comme attributs :
+    -	Un nom
+    -	Une ville
+    -	Une adresse
+    -	Des chambres
+    -	Une piscine ou non
+
+    La classe est pourvue des méthodes suivantes :
+    -	get_dispo() qui renvoie le nombre de chambres disponibles.
+    -	nettoyer() qui fait le ménage dans toutes les chambres de cet hôtel.
+    -	Des méthodes permettant d’accéder à chacun des attributs.
+    """
+
+    def __init__(self, nom:str, ville:str, adresse:str, a_piscine:bool=False):
+        """Créé un nouvel Hotel()"""
+        self.nom = nom
+        self.ville = ville
+        self.adresse = adresse
+        self.chambres = []
+        self.a_piscine = a_piscine
+
+    def get_nom(self):
+        return self.nom
+
+    def get_ville(self):
+        return self.ville
+
+    def get_adresse(self):
+        return self.adresse
+
+    def get_chambres(self):
+        return self.chambres
+
+    def get_a_piscine(self):
+        return self.a_piscine
+
+    def get_dispo(self):
+        chambres_non_occupee = 0
+        for chambre in self.chambres:
+            if not chambre.get_est_occupee():
+                chambres_non_occupee +=1
+        return chambres_non_occupee
+
+    def	nettoyer(self) :
+        """Fait le ménage dans toutes les chambres de cet hôtel"""
+        for chambre in self.chambres:
+            chambre.nettoyer()
+        return None
