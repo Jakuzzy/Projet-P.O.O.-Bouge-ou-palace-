d57dd16a0e18d984b9c5f0016b865daa99d1e90c
CapitaineCouscous <103769157+CapitaineCouscous@users.noreply.github.com>
2022-11-25 09:51:28 +0100

    Update projet.py
    Ajout de plusieurs méthodes dans Hotel, modifications dans les init, début d'un test unitaire fait

--- projet_poo/projet.py
+++ projet_poo/projet.py
@@ -1,4 +1,4 @@
-﻿import comptes_bancaires as cb
+import comptes_bancaires as cb
 
 class Chambre :
     """
@@ -18,7 +18,7 @@ class Chambre :
     -	payer()
     -	Des méthodes permettant d’accéder à chacun des attributs.
     """
-    def __init__(self, numero:int, etage:int, capacite:int, tarif:float, a_vue_mer:bool) :
+    def __init__(self, numero:int, etage:int, capacite:int, tarif:float, a_vue_mer:bool=False) :
         """Créer une nouvelle Chambre()"""
         self.numero = numero
         self.etage = etage
@@ -29,24 +29,31 @@ class Chambre :
         self.a_vue_mer = a_vue_mer
 
     def get_numero(self):
+        """Renvoie le numéro de la chambre"""
         return self.numero
 
     def get_etage(self):
+        """Renvoie l'étage de la chambre"""
         return self.etage
 
     def get_capacite(self):
+        """Renvoie la capacité d'accueil de la chambre"""
         return self.capacite
 
     def get_est_propre(self):
+        """Renvoie si la chambre est propre ou pas"""
         return self.est_propre
 
     def get_est_occupee(self):
+        """Renvoie si la chambre est occupée ou pas"""
         return self.est_occupee
 
     def get_tarif(self):
+        """Renvoie le tarif de la chambre"""
         return self.tarif
 
     def get_a_vue_mer(self):
+        """Renvoie si la chambre a une vue sur la mer ou pas"""
         return self.a_vue_mer
 
     def nettoyer(self) :
@@ -63,11 +70,6 @@ class Chambre :
         """Déclare la chambre non-occupée"""
         self.est_occupee = False
         return None
-
-##    def	payer(self) :
-##        """"""
-##        pass
-##        return None
 
 
 
@@ -86,38 +88,74 @@ class Hotel :
     -	Des méthodes permettant d’accéder à chacun des attributs.
     """
 
-    def __init__(self, nom:str, ville:str, adresse:str, a_piscine:bool=False):
+    def __init__(self, nom:str, ville:str, adresse:str, chambres:list, solde_compte_hotel:int=0 , a_piscine:bool=False):
         """Créé un nouvel Hotel()"""
         self.nom = nom
         self.ville = ville
         self.adresse = adresse
-        self.chambres = []
+        self.chambres = chambres
+        self.compte = cb.CompteBancaire('42',solde_compte_hotel)
         self.a_piscine = a_piscine
 
     def get_nom(self):
+        """Renvoie le nom de l'hôtel"""
         return self.nom
 
     def get_ville(self):
+        """Renvoie la ville de l'hôtel"""
         return self.ville
 
     def get_adresse(self):
+        """Renvoie l'adresse de l'hôtel"""
         return self.adresse
 
     def get_chambres(self):
+        """Renvoie la liste de chambres de l'hôtel"""
         return self.chambres
 
+    def get_compte(self):
+        """Renvoie le compte bancaire de l'hôtel"""
+        return self.compte
+
     def get_a_piscine(self):
+        """Renvoie si l'hôtel a une piscine ou pas"""
         return self.a_piscine
 
+    def get_chambre(self, numero:int, etage:int):
+        """Renvoie la chambre correspondant au numéro et à l'étage donnés"""
+        for chambre in self.chambres:
+            if chambre.numero == numero and chambre.etage == etage:
+                return chambre
+        return False
+
     def get_dispo(self):
+        """Renvoie le nombre de chambres disponibles"""
         chambres_non_occupee = 0
         for chambre in self.chambres:
             if not chambre.get_est_occupee():
                 chambres_non_occupee +=1
         return chambres_non_occupee
 
+    def add_chambres(chambres:list):
+        """Ajoute chambres à la liste de chambres de l'hôtel"""
+        self.chambres += chambres
+
     def	nettoyer(self) :
-        """Fait le ménage dans toutes les chambres de cet hôtel"""
+        """Fait le ménage dans toutes les chambres de l'hôtel"""
         for chambre in self.chambres:
             chambre.nettoyer()
         return None
+
+    def	payer(self, numero:int, etage:int, compte):
+        """Paie la chambre correspondant à l'étage et au numéro donnés avec compte"""
+        chambre = get_chambre(numero,etage)
+        if chambre:
+            compte.transferer(self.compte,chambre.tarif)
+        return None
+
+def test_hotel():
+    liste_chambres = []
+    for i in range(3):
+        for j in range(10):
+            liste_chambres.append(Chambre(j,i,2,78.50,False))
+    pass
